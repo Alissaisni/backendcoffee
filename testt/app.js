961816199bcccaef12c959b03ddd24404ce8328d
alissais <125477768+Alissaisni@users.noreply.github.com>
2024-11-10 08:40:46 +0700

    Add files via upload

---
+++ testt/app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const bodyParser = require('body-parser')
+const menuController = require('./menuController')
+const cors = require('cors');
+app.use(cors({
+  origin: '*', // Allow all origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+}));
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+// set cors all allows
+app.use(bodyParser.json())
+
+app.post("/menus", menuController.createMenu);
+app.get("/menus/:id", menuController.getDetailMenu)
+app.get("/menus", menuController.getAllMenu);
+app.put("/menus/:id", menuController.editMenu);
+app.delete("/menus/:id", menuController.deleteMenu);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ testt/menuController.js
@@ -0,0 +1,88 @@
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+const createMenu = async (req, res) => {
+  const { deskripsi, nama, makanan_pelengkap, kategori } = req.body;
+  try {
+    const menu = await prisma.menus.create({
+      data: {
+        nama,
+        deskripsi,
+        kategori,
+        makanan_pelengkap,
+      },
+    });
+    return res.json(menu);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getAllMenu = async (req, res) => {
+  const menus = await prisma.menus.findMany();
+
+  const responseJSON = {
+    data: menus,
+    status: 200,
+    message: "success",
+  };
+  return res.json(responseJSON);
+};
+
+const editMenu = async (req, res) => {
+  const { id } = req.params;
+  const { deskripsi, nama, makanan_pelengkap, kategori } = req.body;
+  try {
+    const menu = await prisma.menus.update({
+      where: {
+        id: Number(id),
+      },
+      data: {
+        nama,
+        deskripsi,
+        kategori,
+        makanan_pelengkap,
+      },
+    });
+    return res.json(menu);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const deleteMenu = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const menu = await prisma.menus.delete({
+      where: {
+        id: Number(id),
+      },
+    });
+    return res.json(menu);
+  } catch (err) {
+    console.log(err);
+  }
+};
+const getDetailMenu = async (req, res) => {
+  const { id } = req.params;
+  console.log(id)
+  try {
+    const menu = await prisma.menus.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+    return res.json(menu);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+module.exports = {
+  createMenu,
+  getAllMenu,
+  editMenu,
+  deleteMenu,
+  getDetailMenu,
+};
